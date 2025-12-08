bd548db23075f10db4665bf4e52ffee83f7d3dd9
kitros <kitcotros2@gmail.com>
2025-12-08 00:52:51 -0600

    remove unnecessary comments

--- controllers/likes.js
+++ controllers/likes.js
@@ -3,7 +3,7 @@ const ClothingItem = require("../models/clothingItem");
 module.exports.likeItem = (req, res) =>
   ClothingItem.findByIdAndUpdate(
     req.params.itemId,
-    { $addToSet: { likes: req.user._id } }, // add _id to the array if it's not there yet
+    { $addToSet: { likes: req.user._id } },
     { new: true }
   )
     .orFail()
@@ -20,12 +20,11 @@ module.exports.likeItem = (req, res) =>
       }
       return res.status(500).send({ message: err.message });
     });
-// ...
 
 module.exports.dislikeItem = (req, res) =>
   ClothingItem.findByIdAndUpdate(
     req.params.itemId,
-    { $pull: { likes: req.user._id } }, // remove _id from the array
+    { $pull: { likes: req.user._id } },
     { new: true }
   )
     .orFail()
