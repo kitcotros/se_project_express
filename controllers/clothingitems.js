b6be080e5d495fef965542474c9674b2a597ef50
kitros <kitcotros2@gmail.com>
2025-12-08 01:05:21 -0600

    refactor add item response

--- controllers/clothingitems.js
+++ controllers/clothingitems.js
@@ -1,15 +1,11 @@
 const ClothingItem = require("../models/clothingItem");
 
 const createItem = (req, res) => {
-  console.log(req);
-  console.log(req.body);
-
   const { name, weather, imageUrl } = req.body;
 
   ClothingItem.create({ name, weather, imageUrl })
     .then((item) => {
-      console.log(item);
-      res.send({ data: item });
+      res.send(item);
     })
     .catch((err) => {
       res.status(500).send({ message: err.message });
