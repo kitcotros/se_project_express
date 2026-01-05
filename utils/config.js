e3372f49e247f582f224fabbbc2161035abe3e6c
kitros <kitcotros2@gmail.com>
2026-01-04 22:03:03 -0600

    fix secret

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,3 @@
-const { JWT_SECRET = "your_very_long_and_random_secret_key_here" } =
-  process.env;
+const JWT_SECRET = "your_very_long_and_random_secret_key_here";
 
-module.exports = {
-  JWT_SECRET,
-};
+module.exports = JWT_SECRET;
